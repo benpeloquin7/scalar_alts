b10ced840877800b8ec79f1337bd2e1719af1cd1
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-06-23 14:23:52 -0700

    reorg dirs

--- experiments/templates/empirical_alts_template.js
+++ experiments/templates/empirical_alts_template.js
@@ -69,7 +69,7 @@ function shuffle (a) {
 // Current study
 // =========================================================
 
-var DOMAIN = "current_domain"
+var DOMAIN = "restaurant"
 // Stimuli
 // -------
 var sents = {
